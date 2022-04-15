1801e7b12e180a1c6d7804565d87ada023cfcf82
sayrahhh <78211215+sayrahhh@users.noreply.github.com>
2022-04-15 04:59:43 +0100

    Update App.js

--- jsxapp/src/App.js
+++ jsxapp/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Sarah Usman</h1>
+
+        <br />
+
+        <img src={require("./imgInSrc.jfif")} alt="hellosrc" />
+
+        <br />
+
+        <img src="../imageInPublic.jfif" />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
